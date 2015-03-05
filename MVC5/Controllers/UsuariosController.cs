941fa1c83b6082bce4e1094525d22f9772feb562
Nacho1991 <ivalerio2222@hotmail.com>
2015-03-05 15:33:48 -0600

    Proyecto-Ambiente-Web-II
    - Se creo los metodos de registro para el inicio de sesion
    - Se empezo aplicar diseño en el Index, Editar de Pacientes

--- MVC5/Controllers/UsuariosController.cs
+++ MVC5/Controllers/UsuariosController.cs
@@ -1,24 +1,93 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
+using System.Globalization;
 using System.Linq;
-using System.Net;
+using System.Security.Claims;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System.Threading.Tasks;
 using appProyectoFinal.Contexto;
+using System.Net;
 using appProyectoFinal.Models;
+using System.Data.Entity;
 
 namespace appProyectoFinal.Controllers
 {
     public class UsuariosController : Controller
     {
         private ApplicationContext db = new ApplicationContext();
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
 
+        public UsuariosController() { }
+        public UsuariosController(ApplicationUserManager userManager, ApplicationSignInManager signInManager) 
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
         // GET: Usuarios
         public ActionResult Index()
         {
             return View(db.Usuarios.ToList());
+        }
+
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(Usuario model, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            // No cuenta los errores de inicio de sesión para el bloqueo de la cuenta
+            // Para permitir que los errores de contraseña desencadenen el bloqueo de la cuenta, cambie a shouldLockout: true
+            var result = await SignInManager.PasswordSignInAsync(model.Nombre_Usuario, model.Contrasenna, false, shouldLockout: false);
+            switch (result)
+            {
+                case SignInStatus.Success:
+                    return RedirectToLocal(returnUrl);
+                case SignInStatus.LockedOut:
+                    return View("Lockout");
+                case SignInStatus.RequiresVerification:
+                    return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = false });
+                case SignInStatus.Failure:
+                default:
+                    ModelState.AddModelError("", "Intento de inicio de sesión no válido.");
+                    return View(model);
+            }
         }
 
         // GET: Usuarios/Details/5
@@ -45,18 +114,33 @@ namespace appProyectoFinal.Controllers
         // POST: Usuarios/Create
         // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
         // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        // POST: /Account/Register
         [HttpPost]
+        [AllowAnonymous]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Nombre,Nombre_Usuario,Contrasenna")] Usuario usuario)
+        public async Task<ActionResult> Create(Usuario model)
         {
             if (ModelState.IsValid)
             {
-                db.Usuarios.Add(usuario);
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                var user = new ApplicationUser { UserName = model.Nombre_Usuario,  Email = model.Nombre };
+                var result = await UserManager.CreateAsync(user, model.Contrasenna);
+                if (result.Succeeded)
+                {
+                    await  SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+
+                    // Para obtener más información sobre cómo habilitar la confirmación de cuenta y el restablecimiento de contraseña, visite http://go.microsoft.com/fwlink/?LinkID=320771
+                    // Enviar correo electrónico con este vínculo
+                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
+                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                    // await UserManager.SendEmailAsync(user.Id, "Confirmar cuenta", "Para confirmar la cuenta, haga clic <a href=\"" + callbackUrl + "\">aquí</a>");
+
+                    return RedirectToAction("Index", "Usuarios");
+                }
+                AddErrors(result);
             }
 
-            return View(usuario);
+            // Si llegamos a este punto, es que se ha producido un error y volvemos a mostrar el formulario
+            return View(model);
         }
 
         // GET: Usuarios/Edit/5
@@ -124,5 +208,21 @@ namespace appProyectoFinal.Controllers
             }
             base.Dispose(disposing);
         }
+        private ActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
+        }
     }
+
 }
